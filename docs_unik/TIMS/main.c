29bd3fe7a9f9f1a920ff48983840259f8aa6d663
porfyr <rock097abc@gmail.com>
2022-12-24 14:06:28 +0200

    бекап

---
+++ docs_unik/TIMS/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%d\n", 27210048 % 30);
+
+	return 0;
+}
--- go/poligon/main.go
+++ go/poligon/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
-	_ "fmt"
+	//"os"
 )
 
 func main() {
 
-	fmt.Println("Ніби робе")
+	I := Hk{"sn", 8}
+
+	I.print()
+	I.plusYr()
+	I.print()
+
+	//fmt.Println("Ніби робе")
+}
+
+type Hk struct {
+	name string
+	age  int8
+}
+
+func (u Hk) print() {
+	fmt.Printf("%v: %v\n", u.name, u.age)
+}
+
+func (u *Hk) plusYr() {
+	u.age++
 }
